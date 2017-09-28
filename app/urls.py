6227ad61a8d235bf8552e7c03b2d50c8375765a7
Madhurendra Sachan <madhurendra@tikaj.com>
2017-09-28 16:20:01 +0530

    Added get_all Added methods to get system stats

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^search/$', views.search, name='search'),
+    url(r'^get_system_stat/$', views.get_system_stat, name='get_system_stat'),
+    url(r'^get_all/$', views.get_all, name='get_all'),
 ]
--- app/views.py
+++ app/views.py
@@ -23,11 +23,11 @@ def search(request):
     endpoint = 'search/users?q=%s' % (query)
     if bool(sort):
         endpoint = endpoint + ('&sort=%s&order=%s' % (sort, order))
-    req = requests.get('https://api.github.com/'+endpoint)
+    req = requests.get('https://api.github.com/' + endpoint)
     data = json.loads(req.content)
 
     # insert hits to database. if request is successful
-    db['hits'].insert({'time': datetime.datetime.utcnow(),'endpoint':endpoint})
+    db['hits'].insert({'time': datetime.datetime.utcnow(), 'endpoint': endpoint})
 
     for item in data.get("items", []):
         record = {}
@@ -38,6 +38,51 @@ def search(request):
 
     return render(request, 'app/search.html', {'data': data.get("items", [])})
 
+
+def get_stat(collection,field):
+    hits_by_days = [
+        {'$group': {'_id': {
+            'year': {'$year': field},
+            'month': {'$month': field},
+            'day': {'$dayOfMonth': field}
+        },
+            'count': {'$sum': 1}
+        }
+        }
+    ]
+
+    hits_by_week = [
+        {'$group': {'_id': {
+            'week': {'$week': field},
+        },
+            'count': {'$sum': 1}
+        }
+        }
+    ]
+
+    hits_by_months = [
+        {'$group': {'_id': {
+            'month': {'$month': field},
+        },
+            'count': {'$sum': 1}
+        }
+        }
+    ]
+
+    response = {'days':list(collection.aggregate(hits_by_days)),'weeks':list(collection.aggregate(hits_by_week)),'months':list(collection.aggregate(hits_by_months))}
+    return response 
+
+
+def get_system_stat(request):
+    response = {'hits':get_stat(db['hits'],"$time"),
+                'records':get_stat(collection,"$_update_time")}
+    print(response)
+    # create a html to render this json
+    return render(request,"app/stats.html",response)
+
+def get_all(request):
+    users = list(collection.find())
+    return render(request, 'app/search.html', {'data': users})
 
 def profile(request):
     pass
